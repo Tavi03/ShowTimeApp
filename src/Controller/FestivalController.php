6171569b351bb662c1534646ec4287cc6c2da38d
Tavi03 <taviurdu@yahoo.com>
2025-07-04 14:33:22 +0300

    -Implemented Band and Festival CRUD -Installed and implemented DaisyUI theme

---
+++ src/Controller/FestivalController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Festival;
+use App\Form\FestivalType;
+use App\Repository\FestivalRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FestivalController extends AbstractController
+{
+    #[Route('/festival', name: 'app_festival_index', methods: ['GET'])]
+    public function index(FestivalRepository $festivalRepository): Response
+    {
+        return $this->render('festival/index.html.twig', [
+            'controller_name' => 'FestivalController',
+            'festivals' => $festivalRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_festival_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $festival = new Festival();
+        $form = $this->createForm(FestivalType::class, $festival);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($festival);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_festival_index');
+        }
+
+        return $this->render('festival/new.html.twig', [
+            'festival' => $festival,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_festival_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Festival $festival, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FestivalType::class, $festival);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_festival_index');
+        }
+
+        return $this->render('festival/edit.html.twig', [
+            'festival' => $festival,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_festival_delete', methods: ['POST'])]
+    public function delete(Request $request, Festival $festival, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $festival->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($festival);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_festival_index');
+    }
+}
